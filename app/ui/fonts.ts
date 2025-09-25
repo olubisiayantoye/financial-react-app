43d4295d2b9a442d0f3e69d8562a4d68dfb8dc90
olubisiayantoye <olubisiayantoye@gmail.com>
2025-09-25 18:09:40 +0100

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// Inter font
+export const inter = Inter({ subsets: ['latin'] });
+
+// Lusitana font
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'], // adjust weights if needed
+});
