149111dde191b078d213d2d8b8be7cb5ec74fa0a
olubisiayantoye <olubisiayantoye@gmail.com>
2025-09-29 16:30:20 +0100

    new file:   app/dashboard/customers/page.tsx 	new file:   app/dashboard/invoices/page.tsx 	new file:   app/dashboard/layout.tsx 	modified:   app/layout.tsx 	modified:   app/page.tsx 	modified:   app/ui/acme-logo.tsx 	modified:   app/ui/dashboard/nav-links.tsx 	modified:   app/ui/fonts.ts 	deleted:    nextjs-dashboard/node_modules/.pnpm/path-parse@1.0.7/node_modules/path-parse_tmp_1768/LICENSE 	deleted:    nextjs-dashboard/node_modules/.pnpm/path-parse@1.0.7/node_modules/path-parse_tmp_1768/README.md 	deleted:    nextjs-dashboard/node_modules/.pnpm/path-parse@1.0.7/node_modules/path-parse_tmp_1768/index.js 	deleted:    nextjs-dashboard/node_modules/.pnpm/path-parse@1.0.7/node_modules/path-parse_tmp_1768/package.json 	deleted:    nextjs-dashboard/node_modules/.pnpm/postcss-js@4.0.1_postcss@8.5.1/node_modules/postcss-js_tmp_1768/LICENSE 	deleted:    nextjs-dashboard/node_modules/.pnpm/postcss-js@4.0.1_postcss@8.5.1/node_modules/postcss-js_tmp_1768/README.md 	deleted:    nextjs-dashboard/node_modules/.pnpm/postcss-js@4.0.1_postcss@8.5.1/node_modules/postcss-js_tmp_1768/async.js 	deleted:    nextjs-dashboard/node_modules/.pnpm/postcss-js@4.0.1_postcss@8.5.1/node_modules/postcss-js_tmp_1768/index.js 	deleted:    nextjs-dashboard/node_modules/.pnpm/postcss-js@4.0.1_postcss@8.5.1/node_modules/postcss-js_tmp_1768/index.mjs 	deleted:    nextjs-dashboard/node_modules/.pnpm/postcss-js@4.0.1_postcss@8.5.1/node_modules/postcss-js_tmp_1768/objectifier.js 	deleted:    nextjs-dashboard/node_modules/.pnpm/postcss-js@4.0.1_postcss@8.5.1/node_modules/postcss-js_tmp_1768/package.json 	deleted:    nextjs-dashboard/node_modules/.pnpm/postcss-js@4.0.1_postcss@8.5.1/node_modules/postcss-js_tmp_1768/parser.js 	deleted:    nextjs-dashboard/node_modules/.pnpm/postcss-js@4.0.1_postcss@8.5.1/node_modules/postcss-js_tmp_1768/process-result.js 	deleted:    nextjs-dashboard/node_modules/.pnpm/postcss-js@4.0.1_postcss@8.5.1/node_modules/postcss-js_tmp_1768/sync.js 	deleted:    nextjs-dashboard/node_modules/.pnpm/supports-preserve-symlinks-flag@1.0.0/node_modules/supports-preserve-symlinks-flag_tmp_1768/.eslintrc 	deleted:    nextjs-dashboard/node_modules/.pnpm/supports-preserve-symlinks-flag@1.0.0/node_modules/supports-preserve-symlinks-flag_tmp_1768/.github/FUNDING.yml 	deleted:    nextjs-dashboard/node_modules/.pnpm/supports-preserve-symlinks-flag@1.0.0/node_modules/supports-preserve-symlinks-flag_tmp_1768/.nycrc 	deleted:    nextjs-dashboard/node_modules/.pnpm/supports-preserve-symlinks-flag@1.0.0/node_modules/supports-preserve-symlinks-flag_tmp_1768/CHANGELOG.md 	deleted:    nextjs-dashboard/node_modules/.pnpm/supports-preserve-symlinks-flag@1.0.0/node_modules/supports-preserve-symlinks-flag_tmp_1768/LICENSE 	deleted:    nextjs-dashboard/node_modules/.pnpm/supports-preserve-symlinks-flag@1.0.0/node_modules/supports-preserve-symlinks-flag_tmp_1768/README.md 	deleted:    nextjs-dashboard/node_modules/.pnpm/supports-preserve-symlinks-flag@1.0.0/node_modules/supports-preserve-symlinks-flag_tmp_1768/browser.js 	deleted:    nextjs-dashboard/node_modules/.pnpm/supports-preserve-symlinks-flag@1.0.0/node_modules/supports-preserve-symlinks-flag_tmp_1768/index.js 	deleted:    nextjs-dashboard/node_modules/.pnpm/supports-preserve-symlinks-flag@1.0.0/node_modules/supports-preserve-symlinks-flag_tmp_1768/package.json 	deleted:    nextjs-dashboard/node_modules/.pnpm/supports-preserve-symlinks-flag@1.0.0/node_modules/supports-preserve-symlinks-flag_tmp_1768/test/index.js

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,10 +1,4 @@
 import { Inter, Lusitana } from 'next/font/google';
 
-// Inter font
 export const inter = Inter({ subsets: ['latin'] });
-
-// Lusitana font
-export const lusitana = Lusitana({
-  subsets: ['latin'],
-  weight: ['400', '700'], // adjust weights if needed
-});
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
